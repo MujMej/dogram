0a345c2e75ccaada655a9a590422c0530bde1f39
MujMej <130869184+MujMej@users.noreply.github.com>
2026-03-31 00:43:15 +0200

    Add Firebase initialization and configuration

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "OVDJE_IDE",
+  authDomain: "OVDJE_IDE",
+  projectId: "OVDJE_IDE",
+  storageBucket: "OVDJE_IDE",
+  messagingSenderId: "OVDJE_IDE",
+  appId: "OVDJE_IDE"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
