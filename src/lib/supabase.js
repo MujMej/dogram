ebf085a9d66f870008096db6cc1f90b60b3425a3
MujMej <130869184+MujMej@users.noreply.github.com>
2026-04-04 02:19:07 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rkrkpsqkniesgnwgoxpm.supabase.co'
-const supabaseAnonKey = 'sb_publishable_8N_ZF_jmB0EQIX1ppfdcIw_p_nDbOs9'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
